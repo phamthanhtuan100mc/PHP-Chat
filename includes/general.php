234538f7524a7a8fa6969940897703b2c169d6a2
DESKTOP-2PH6C2A\tuan.PT <phamthanhtuan100mc@gmail.com>
2021-05-24 17:14:18 +0700

    fix set timezone

--- includes/general.php
+++ includes/general.php
@@ -3,7 +3,7 @@
 // Kết nối với file connectdb.php
 include_once("connectdb.php");
 // Lấy múi giờ chung cho site
-date_default_timezone_get("Asia/Ho_Chi_Minh");
+date_default_timezone_set("Asia/Ho_Chi_Minh");
 $date_current = "";
 $date_current = date("Y-m-d H:i:sa");
 
