27a9ed9960f1f3c77a117270252cc21be77dfab6
DESKTOP-2PH6C2A\tuan.PT <phamthanhtuan100mc@gmail.com>
2021-05-24 17:37:52 +0700

    add login form

--- index.php
+++ index.php
@@ -4,6 +4,19 @@
 include('includes/general.php');
 // Kết nối header
 include('includes/header.php');
+?>
+<div class="box-join">
+    <h2>Đăng ký/ Đăng nhập</h2>
+    <form method="POST" id="formJoin" onsubmit="return false;">
+        <input type="text" placeholder="Tên đăng nhập" class="data-input" id="username" >
+        <br />
+        <input type="password" placeholder="Mật khẩu" class="data-input" id="password" >
+        <br />
+        <button class="btn-submit">Bắt đầu</button>
+        <div class="alert danger"></div>
+    </form>
+</div>
+<?php
 // Kết nối footer
 include('includes/footer.php');
 
