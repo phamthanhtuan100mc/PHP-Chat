995fa7d2144377545b492a899ad43db17605825a
DESKTOP-2PH6C2A\tuan.PT <phamthanhtuan100mc@gmail.com>
2021-05-25 18:19:02 +0700

    debug deploy mode

--- includes/general.php
+++ includes/general.php
@@ -5,7 +5,7 @@ include_once("connectdb.php");
 // Lấy múi giờ chung cho site
 date_default_timezone_set("Asia/Ho_Chi_Minh");
 $date_current = "";
-$date_current = date("Y-m-d H:i:sa");
+$date_current = date("Y-m-d H:i:s");
 
 // Bắt đầu lưu session
 session_start();
